6c8404c139c24da1e5940ea01efd73f085e8fa7b
Ilham Firmansyah <iyamif@gmail.com>
2026-08-13 18:04:50 +0700

    update db

--- backend/config/database.go
+++ backend/config/database.go
@@ -4,54 +4,73 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"mini-paylater/model"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-// memuat variabel lingkungan dari file .env
-func LoadEnv() {
-	// Membaca file .env di root direktori
+func ConnectDatabase() *gorm.DB {
+
 	err := godotenv.Load()
 
-	// Jika file .env tidak ditemukan, tampilkan pesan peringatan
 	if err != nil {
 		log.Println("Warning: .env file not found")
 	}
-}
 
-// membuat koneksi ke database PostgreSQL menggunakan GORM
-func ConnectDatabase() *gorm.DB {
-	// Mengambil kredensial database dari variabel lingkungan (env)
-	host := os.Getenv("DB_HOST")         // Host server database (misal: localhost atau host docker)
-	port := os.Getenv("DB_PORT")         // Port database PostgreSQL (default: 5432)
-	user := os.Getenv("DB_USER")         // Username database
-	password := os.Getenv("DB_PASSWORD") // Password database
-	dbname := os.Getenv("DB_NAME")       // Nama database
-	sslmode := os.Getenv("DB_SSLMODE")   // Mode SSL (misal: disable atau require)
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
 
-	// Format string DSN (Data Source Name) untuk PostgreSQL
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		host,
+		port,
 		user,
 		password,
 		dbname,
-		port,
 		sslmode,
 	)
 
-	// Membuka koneksi database dengan driver postgres
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{
+			PrepareStmt: false,
+		},
+	)
 
-	// Jika koneksi gagal, hentikan aplikasi (fatal log)
 	if err != nil {
 		log.Fatal("Failed to connect database:", err)
 	}
 
 	log.Println("Database connected successfully")
 
+	sqlDB, err := db.DB()
+
+	if err != nil {
+		log.Fatal("Failed to get database instance:", err)
+	}
+
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetMaxOpenConns(20)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	err = db.AutoMigrate(
+		&model.Loan{},
+		&model.Installment{},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
+	log.Println("Database migration completed")
+
 	return db
 }
-
