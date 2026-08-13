72dd1c1a120309ffa3b3007228ba646b747e0b53
Ilham Firmansyah <iyamif@gmail.com>
2026-08-13 23:23:05 +0700

    update

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HyCyr8lWq9tiIewWbVOtsh5nFVREEUrP57FJsdgqW34=\"\n}"
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mtZTTslk9BfkOh8J5xU/eTlHrN3AUfoBOG7COBq5D0k=\"\n}"
